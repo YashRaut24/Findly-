2d1703639b2b84100951956b7999f06cf7f36197
YashRaut24 <yashcodezzzz24@gmail.com>
2026-06-13 17:17:19 +0530

    Did environment setup with groq sdk

---
+++ test_setup.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+user_input = input("Ask something: ")     
+
+message = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {"role": "user", "content": user_input}     
+    ]
+)
+
+print(message.choices[0].message.content)
